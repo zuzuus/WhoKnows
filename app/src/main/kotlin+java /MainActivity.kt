500716a05845b78c287929aa99833b27d1c26bfe
whwjbdxuwbrvsheh <205212565+whwjbdxuwbrvsheh@users.noreply.github.com>
2026-09-24 14:43:53 +0800

    Implement MainActivity with lifecycle logging and navigation

--- app/src/main/kotlin+java /MainActivity.kt
+++ app/src/main/kotlin+java /MainActivity.kt
@@ -1,1 +1,72 @@
+package com.example.studentquizapplication
 
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.databinding.DataBindingUtil
+import com.example.studentquizapplication.databinding.ActivityMainBinding
+import kotlin.jvm.java
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "Lifecycle_MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate Callback invoked")
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnStart.setOnClickListener {
+            val name = binding.etName.text.toString().trim()
+
+            if (name.isEmpty()) {
+                Toast.makeText(
+                    this,
+                    "Please enter your name!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            val bundle = Bundle().apply {
+                putString("EXTRA_NAME", name)
+            }
+
+            val intent = Intent(this, QuizActivity::class.java).apply {
+                putExtras(bundle)
+            }
+
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Callback invoked")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Callback invoked")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Callback invoked")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Callback invoked")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Callback invoked")
+    }
+}
